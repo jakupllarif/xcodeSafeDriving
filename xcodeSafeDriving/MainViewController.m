d7f8f08b853407d38ef6909821b47d61a27b7376
gabrielliu <gabrielliu@Gabriels-MacBook-Pro-2.local>
2014-02-01 13:03:24 -0500

    MainViewController title mapping data done

--- xcodeSafeDriving/MainViewController.m
+++ xcodeSafeDriving/MainViewController.m
@@ -7,7 +7,33 @@
 //
 
 #import "MainViewController.h"
+#import <Parse/Parse.h>
+
+@interface MainViewController ()
+
+@end
 
 @implementation MainViewController
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
 
+- (void)viewDidAppear:(BOOL)animated{
+    PFUser *currentuser = [PFUser currentUser];
+    if (currentuser) {
+        //NSString *firstName = [currentuser objectForKey:@"firstName"];
+        NSString *welcomeMessage = [NSString stringWithFormat:@"Welcome %@ %@", [currentuser objectForKey:@"firstName"], [currentuser objectForKey:@"lastName"]];
+        _welcomeMessage.text = welcomeMessage;
+       // _welcomeMessage.text = [NSString stringWithFormat:@"Welcome %@ %@", [currentuser objectForKey:@"firstName"], [currentuser objectForKey:@"lastName"]];
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 @end
