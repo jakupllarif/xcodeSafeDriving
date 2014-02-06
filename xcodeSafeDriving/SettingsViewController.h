9bf67347676b75c1752b97f7b523ad45a1c57493
gabrielliu <gabrielliu@Gabriels-MacBook-Pro-2.local>
2014-02-05 21:23:03 -0500

    Revert cb55bee..7068b75
    This rolls back to commit cb55bee8c4571b3068c2e3d740577d4ba00c75d3.

---
+++ xcodeSafeDriving/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  xcodeSafeDriving
+//
+//  Created by Flodjana Jakupllari on 2/5/14.
+//  Copyright (c) 2014 Jakupllari F. & Liu Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+@end
