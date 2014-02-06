44e913ab08b4614e93293d76214c0e4c5fe315de
Flodjana Jakupllari <Ajakupllari@Ardits-MacBook-Pro.local>
2014-02-05 20:13:11 -0500

    settings view

--- xcodeSafeDriving/SettingsViewController.h
+++ xcodeSafeDriving/SettingsViewController.h
@@ -2,13 +2,22 @@
 //  SettingsViewController.h
 //  xcodeSafeDriving
 //
-//  Created by Gabriel Liu on 1/29/14.
+//  Created by Flodjana Jakupllari on 2/2/14.
 //  Copyright (c) 2014 Jakupllari F. & Liu Z. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface SettingsViewController : UIViewController
+@interface SettingsViewController : UITableViewController
 - (IBAction)logout:(id)sender;
+@property (weak, nonatomic) IBOutlet UITableView *settingsTableController;
+- (IBAction)drunkBtn:(id)sender;
+- (IBAction)overspeedBtn:(id)sender;
+- (IBAction)messageBlockBtn:(id)sender;
+- (IBAction)emergencyBtn:(id)sender;
+@property (weak, nonatomic) IBOutlet UISwitch *drunkBtnOutlet;
+@property (weak, nonatomic) IBOutlet UISwitch *overspeedBtnOutlet;
+@property (weak, nonatomic) IBOutlet UISwitch *messageBlockBtnOutlet;
+@property (weak, nonatomic) IBOutlet UISwitch *emergencyBtnOutlet;
 
 @end
