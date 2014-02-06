aa61779978924748171f25139e107e4b0de11b09
gabrielliu <gabrielliu@Gabriels-MacBook-Pro-2.local>
2014-02-05 20:27:39 -0500

    change settingViewController to Table view controller

---
+++ xcodeSafeDriving/SettingViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.m
+//  xcodeSafeDriving
+//
+//  Created by Gabriel Liu on 2/5/14.
+//  Copyright (c) 2014 Jakupllari F. & Liu Z. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@implementation SettingViewController
+
+@end
